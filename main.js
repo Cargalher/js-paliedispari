26cfcdfcbddffc5c1d8a44c796af435452bab846
Cargalher <cargalher@gmail.com>
2021-04-29 16:37:11 +0200

    promt printed to the console

--- main.js
+++ main.js
@@ -3,4 +3,4 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 var word = prompt("Write a word!");
-console.log(word);
+console.log("Let's check if your word: " + ""+ word + ""+ " is a palindrome.");
