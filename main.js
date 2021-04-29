5e373a9402193e1d27b7901722ba299649f612af
Cargalher <cargalher@gmail.com>
2021-04-29 16:40:18 +0200

    function sintax added

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // Palidroma:
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
 
+// Chiedere all’utente di inserire una parola
 var word = prompt("Write a word!");
 console.log("Let's check if your word: " + ""+ word + ""+ " is a palindrome.");
+
+
+// Creare una funzione per capire se la parola inserita è palindroma
+function name (){
+    
+}
